c4e09c7aeba84f9b3780e1f4a70792491c315436
Andrea Kim <andreaforanimals@gmail.com>
2017-08-28 20:41:33 -0500

    fixed intents and extras

--- app/src/main/java/com/example/akime/todosandreminders/EditItemActivity.java
+++ app/src/main/java/com/example/akime/todosandreminders/EditItemActivity.java
@@ -3,15 +3,15 @@ package com.example.akime.todosandreminders;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-
 public class EditItemActivity extends AppCompatActivity {
     Button btnEditSubmit;
-    String itemToEdit;
-
+    String editable;
+    Integer pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +20,27 @@ public class EditItemActivity extends AppCompatActivity {
         EditText etMultiLine = (EditText) findViewById(R.id.etEditTextMultiline);
         btnEditSubmit = (Button) findViewById(R.id.btnSave);
 
-        itemToEdit = getIntent().getStringExtra("listItem");
-        etMultiLine.setText(itemToEdit);
-        final int position = getIntent().getIntExtra("position", 0);
-
+        editable = getIntent().getStringExtra("listItem");
+        pos = getIntent().getExtras().getInt("position");
+        etMultiLine.setText(editable);
+        etMultiLine.setSelection(etMultiLine.getText().length());
 
         btnEditSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onSubmit(btnEditSubmit);
+                EditText et = (EditText) findViewById(R.id.etEditTextMultiline);
+                String str = et.getText().toString();
+                Log.d("lihla", "onClick : "+str);
+                Intent backToMainActivityIntent = new Intent(EditItemActivity.this, MainActivity.class);
+                backToMainActivityIntent.putExtra("listItem", str);
+                backToMainActivityIntent.putExtra("position", pos);
+
+                setResult(RESULT_OK, backToMainActivityIntent);
+                finish();
             }
         });
+
     }
-
-    public void onSubmit(View v) {
-        Intent data = new Intent(EditItemActivity.this, MainActivity.class);
-        data.putExtra("editedItem", 0);
-        setResult(RESULT_OK, data);
-        startActivity(data);
-        this.finish();
-    }
-
-
 
 }
+
--- app/src/main/java/com/example/akime/todosandreminders/MainActivity.java
+++ app/src/main/java/com/example/akime/todosandreminders/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -18,76 +20,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> todoItems;
-    ArrayAdapter<String> aTodoAdapter;
-    ListView lvItems;
-    EditText etEditText;
-    Intent editTextIntent;
+    ArrayList<String> mTodoItems;
+    ArrayAdapter<String> mTodoAdapter;
+    ListView mLvItems;
+    String mLvItem;
+    EditText mEditText;
+    Intent mIntent;
     private final int REQUEST_CODE = 20;
-
+    String mEditable;
+    Integer mPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvItems = (ListView) findViewById(R.id.lvItems);
+        mLvItems = (ListView) findViewById(R.id.lvItems);
         populateArrayItems();
-        lvItems.setAdapter(aTodoAdapter);
-        etEditText = (EditText) findViewById(R.id.etEditText);
-        editTextIntent = new Intent(this, EditItemActivity.class);
-        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        mLvItems.setAdapter(mTodoAdapter);
+
+        mEditText = (EditText) findViewById(R.id.etEditText);
+        mIntent = new Intent(this, EditItemActivity.class);
+
+        mLvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                todoItems.remove(position);
-                aTodoAdapter.notifyDataSetChanged();
+                mTodoItems.remove(position);
+                mTodoAdapter.notifyDataSetChanged();
                 writeItems();
                 return true;
             }
         });
 
-        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mLvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final String listItem = todoItems.get(position);
-                editTextIntent.putExtra("listItem",listItem);
-                editTextIntent.putExtra("position",position);
-                startActivityForResult(editTextIntent, REQUEST_CODE);
+                mLvItem = mTodoItems.get(position);
+
+                mIntent.putExtra("listItem", mLvItem);
+                Log.i("lihla", "List Item = " + mLvItem);
+
+                mIntent.putExtra("position", position);
+                Log.i("lihla", "position = " + position);
+                startActivityForResult(mIntent, REQUEST_CODE);
             }
-
         });
-
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-
-            String editedItem = data.getExtras().getString("editedItem");
-//            int code = data.getExtras().getInt("code", 0);
-
-        }
-    }
-
-
-
-    public void launchEditActivity() {
-        Intent editTextIntent = new Intent(MainActivity.this, EditItemActivity.class);
-        editTextIntent.putExtra("toEdit", "listItem");
-        startActivity(editTextIntent);
-    }
-
 
     public void populateArrayItems() {
-        todoItems = new ArrayList<String>();
+//        todoItems = new ArrayList<String>();
         readItems();
-        aTodoAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,todoItems);
+        mTodoAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mTodoItems);
     }
 
     private void readItems() {
         File filesDir = getFilesDir();
         File file = new File(filesDir, "todo.txt");
         try {
-            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+            mTodoItems = new ArrayList<String>(FileUtils.readLines(file));
         } catch (IOException e) {
 
         }
@@ -97,15 +86,36 @@ public class MainActivity extends AppCompatActivity {
         File filesDir = getFilesDir();
         File file = new File(filesDir, "todo.txt");
         try {
-            FileUtils.writeLines(file, todoItems);
-        } catch (IOException e) {
 
+            if (file.exists()){
+                file.delete();
+            }
+            FileUtils.writeLines(file, mTodoItems);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void onAddItem(View view) {
-        aTodoAdapter.add(etEditText.getText().toString());
-        etEditText.setText("");
+        mTodoAdapter.add(mEditText.getText().toString());
+        mEditText.setText("");
         writeItems();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("lihla", "onActivityResult : "+requestCode+", "+resultCode);
+
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            Log.i("lihla", "onActivityResult 1");
+            mLvItem = data.getExtras().getString("listItem");
+            mPosition = data.getExtras().getInt("position");
+            Log.i("lihla", "onActivityResult 2 : **** item brought back is "+ mLvItem + " + position "+ mPosition+ "************");
+
+            // Toast the name to display temporarily on screen
+            Toast.makeText(this, mEditable, Toast.LENGTH_SHORT).show();
+        }
     }
 }
